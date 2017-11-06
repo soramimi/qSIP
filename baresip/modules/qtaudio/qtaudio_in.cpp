6d1a29c1da40a2cdc45b512aab554b28e4c9d63a
soramimi <fi7s-fct@asahi-net.or.jp>
2017-11-06 16:59:49 +0900

    Qt Audio 対応

--- baresip/modules/qtaudio/qtaudio_in.cpp
+++ baresip/modules/qtaudio/qtaudio_in.cpp
@@ -9,38 +9,78 @@
 #include <new>
 #include <re.h>
 #include <rem.h>
-#include <windows.h>
-#include <mmsystem.h>
 #include <baresip.h>
 #include "qtaudio.h"
+
+#include <QAudioInput>
+#include <QDebug>
+#include <QThread>
+#include <memory>
 
 #define DEBUG_MODULE "qtaudio"
 #define DEBUG_LEVEL 5
 #include <re_dbg.h>
 
-#include <QAudioInput>
+class QtAudioSource : public QThread {
+private:
+	int frame_size;
 
-#define READ_BUFFERS   4
-#define INC_RPOS(a) ((a) = (((a) + 1) % READ_BUFFERS))
-
-class QtAudioSource {
-public:
-	struct dspbuf bufs[READ_BUFFERS];
-	int pos;
-	HWAVEIN wavein;
-	volatile bool rdy;
-	volatile bool processing;
-	ausrc_read_h *rh;
+	ausrc_read_h *sink;
 	void *arg;
 	user_filter_fn user_filter;
 	void *user_cookie;
 
-	QAudioInput input;
+	std::shared_ptr<QAudioInput> input;
+	QIODevice *device;
+protected:
+	void run()
+	{
+		std::vector<char> buf(frame_size * 2);
+		while (1) {
+			if (isInterruptionRequested()) break;
 
-	int add_wave_in();
-	static void CALLBACK waveInCallback_(HWAVEOUT hwo, UINT uMsg, DWORD_PTR dwInstance, DWORD_PTR dwParam1, DWORD_PTR dwParam2);
-	int read_stream_open(unsigned int dev, const ausrc_prm *prm);
-	void waveInCallback(HWAVEOUT hwo, UINT uMsg, DWORD_PTR dwParam1, DWORD_PTR dwParam2);
+			int len = buf.size();
+			if (input->bytesReady() < len) {
+				msleep(1);
+			} else {
+				char *p = &buf[0];
+				len = device->read(p, len);
+
+				if (user_filter) {
+					int16_t *p = (int16_t *)p;
+					int n = len / 2;
+					user_filter(user_cookie, p, n);
+				}
+
+				sink((uint8_t *)p, len, arg);
+			}
+		}
+	}
+public:
+	~QtAudioSource()
+	{
+		requestInterruption();
+		wait();
+	}
+
+	void go(ausrc_read_h *sink, void *arg, int frame_size, user_extra_data_t *user_data)
+	{
+		this->sink = sink;
+		this->arg = arg;
+		this->frame_size = frame_size;
+		this->user_filter = user_data ? user_data->filter : nullptr;
+		this->user_cookie = user_data ? user_data->cookie : nullptr;
+		QAudioFormat format;
+		format.setByteOrder(QAudioFormat::LittleEndian);
+		format.setChannelCount(1);
+		format.setCodec("audio/pcm");
+		format.setSampleRate(8000);
+		format.setSampleSize(16);
+		format.setSampleType(QAudioFormat::SignedInt);
+		input = std::shared_ptr<QAudioInput>(new QAudioInput(format));
+		device = input->start();
+		start();
+	}
 };
 
 struct ausrc_st {
@@ -52,213 +92,28 @@ struct ausrc_st {
 static void ausrc_destructor(void *arg)
 {
 	ausrc_st *st = (ausrc_st *)arg;
-	int i;
-
-	/* Mark the device for closing
-	 */
-	st->obj.rdy = false;
-	// pause if callback is processed at the moment
-	// (hazard with st->rh, possible hangup in waveInReset)
-	while (st->obj.processing) {
-    	Sleep(10);
-	}
-
-	st->obj.rh = NULL;
-
-	if (st->obj.wavein) {
-		waveInStop(st->obj.wavein);
-		// WIM_DATA may be sent
-		waveInReset(st->obj.wavein);
-
-		for (i = 0; i < READ_BUFFERS; i++) {
-			waveInUnprepareHeader(st->obj.wavein, &st->obj.bufs[i].wh, sizeof(WAVEHDR));
-			mem_deref(st->obj.bufs[i].mb);
-		}
-	}
-
-	waveInClose(st->obj.wavein);
-
 	mem_deref(st->as);
-
 	st->~ausrc_st();
-}
-
-int QtAudioSource::add_wave_in()
-{
-	struct dspbuf *db = &bufs[pos];
-	WAVEHDR *wh = &db->wh;
-	MMRESULT res;
-
-	wh->lpData          = (LPSTR)db->mb->buf;
-	wh->dwBufferLength  = db->mb->size;
-	wh->dwBytesRecorded = 0;
-	wh->dwFlags         = 0;
-	wh->dwUser          = (DWORD_PTR)db->mb;
-
-	if (wavein) {
-		waveInPrepareHeader(wavein, wh, sizeof(*wh));
-		res = waveInAddBuffer(wavein, wh, sizeof(*wh));
-		if (res != MMSYSERR_NOERROR) {
-			DEBUG_WARNING("add_wave_in: waveInAddBuffer fail: %08x\n", res);
-			return ENOMEM;
-		}
-	}
-
-	INC_RPOS(pos);
-
-	return 0;
-}
-
-void QtAudioSource::waveInCallback(HWAVEOUT hwo, UINT uMsg, DWORD_PTR dwParam1, DWORD_PTR dwParam2)
-{
-	WAVEHDR *wh = (WAVEHDR *)dwParam1;
-
-	(void)hwo;
-	(void)dwParam2;
-
-	if (!rh) return;
-
-	switch (uMsg) {
-	case WIM_CLOSE:
-		rdy = false;
-		break;
-
-	case WIM_OPEN:
-		rdy = true;
-		break;
-
-	case WIM_DATA:
-		processing = true;
-		if (rdy) {
-			if (wavein) {
-				waveInUnprepareHeader(wavein, wh, sizeof(*wh));
-			} else {
-				int16_t *p = (int16_t *)wh->lpData;
-				int n = wh->dwBytesRecorded / 2;
-				for (int i = 0; i < n; i++) {
-					p[i] = 0;
-				}
-			}
-
-			if (user_filter) {
-				int16_t *p = (int16_t *)wh->lpData;
-				int n = wh->dwBytesRecorded / 2;
-				user_filter(user_cookie, p, n);
-			}
-
-			rh((uint8_t *)wh->lpData, wh->dwBytesRecorded, arg);
-
-			add_wave_in();
-		}
-		processing = false;
-		break;
-
-	default:
-		break;
-	}
-}
-
-void CALLBACK QtAudioSource::waveInCallback_(HWAVEOUT hwo, UINT uMsg, DWORD_PTR dwInstance, DWORD_PTR dwParam1, DWORD_PTR dwParam2)
-{
-	QtAudioSource *obj = (QtAudioSource *)dwInstance;
-	obj->waveInCallback(hwo, uMsg, dwParam1, dwParam2);
-}
-
-static unsigned int find_dev(const char *name)
-{
-	WAVEINCAPS wic;
-	unsigned int i;
-	unsigned int nInDevices = waveInGetNumDevs();
-	for (i = 0; i < nInDevices; i++) {
-		if (waveInGetDevCaps(i, &wic, sizeof(WAVEINCAPS))==MMSYSERR_NOERROR) {
-			if (!strcmp(name, wic.szPname)) {
-				return i;
-			}
-		}
-	}
-	return WAVE_MAPPER;
-}
-
-int QtAudioSource::read_stream_open(unsigned int dev, const struct ausrc_prm *prm)
-{
-	WAVEFORMATEX wfmt;
-	MMRESULT res;
-	int i, err = 0;
-
-	/* Open an audio INPUT stream. */
-	wavein = NULL;
-	pos = 0;
-	rdy = false;
-	processing = false;
-
-	for (i = 0; i < READ_BUFFERS; i++) {
-		memset(&bufs[i].wh, 0, sizeof(WAVEHDR));
-		bufs[i].mb = mbuf_alloc(2 * prm->frame_size);
-		if (!bufs[i].mb)
-			return ENOMEM;
-	}
-
-	wfmt.wFormatTag      = WAVE_FORMAT_PCM;
-	wfmt.nChannels       = prm->ch;
-	wfmt.nSamplesPerSec  = prm->srate;
-	wfmt.wBitsPerSample  = 16;
-	wfmt.nBlockAlign     = (prm->ch * wfmt.wBitsPerSample) / 8;
-	wfmt.nAvgBytesPerSec = wfmt.nSamplesPerSec * wfmt.nBlockAlign;
-	wfmt.cbSize          = 0;
-
-	res = waveInOpen(&wavein, dev, &wfmt,
-			  (DWORD_PTR) QtAudioSource::waveInCallback_,
-			  (DWORD_PTR) this,
-			  CALLBACK_FUNCTION | WAVE_FORMAT_DIRECT);
-	if (res != MMSYSERR_NOERROR) {
-		DEBUG_WARNING("waveInOpen: failed %d\n", err);
-//		return EINVAL;
-		wavein = NULL;
-	}
-
-	/* Prepare enough IN buffers to suite at least 50ms of data */
-	for (i = 0; i < READ_BUFFERS; i++)
-		err |= add_wave_in();
-
-	if (wavein) {
-		waveInStart(wavein);
-	}
-
-	return err;
 }
 
 int qtaudio_src_alloc(ausrc_st **stp, struct ausrc *as, struct media_ctx **ctx, ausrc_prm *prm, const char *device, ausrc_read_h *rh, ausrc_error_h *errh, void *arg, user_extra_data_t *user_data)
 {
-	ausrc_st *st;
-	int err;
-
 	(void)ctx;
 	(void)device;
 	(void)errh;
 
-	if (!stp || !as || !prm)
-		return EINVAL;
+	if (!stp || !as || !prm) return EINVAL;
 
-	st = (ausrc_st *)mem_zalloc(sizeof(*st), ausrc_destructor);
-	if (!st)
-		return ENOMEM;
-
-	new(st) ausrc_st();
-
-	st->as  = (struct ausrc *)mem_ref(as);
-	st->obj.rh  = rh;
-	st->obj.arg = arg;
-	st->obj.user_filter = user_data ? user_data->filter : NULL;
-	st->obj.user_cookie = user_data ? user_data->cookie : NULL;
+	ausrc_st *st = (ausrc_st *)mem_zalloc(sizeof(*st), ausrc_destructor);
+	if (!st) return ENOMEM;
 
 	prm->fmt = AUFMT_S16LE;
 
-	err = st->obj.read_stream_open(find_dev(device), prm);
+	new(st) ausrc_st();
+	st->as  = (struct ausrc *)mem_ref(as);
+	st->obj.go(rh, arg, prm->frame_size, user_data);
 
-	if (err)
-		mem_deref(st);
-	else
-		*stp = st;
+	*stp = st;
 
-	return err;
+	return 0;
 }
--- baresip/modules/qtaudio/qtaudio_out.cpp
+++ baresip/modules/qtaudio/qtaudio_out.cpp
@@ -9,37 +9,64 @@
 #include <new>
 #include <re.h>
 #include <rem.h>
-#include <windows.h>
-#include <mmsystem.h>
 #include <baresip.h>
 #include "qtaudio.h"
+
+#include <QAudioOutput>
+#include <QThread>
+#include <memory>
 
 #define DEBUG_MODULE "qtaudio"
 #define DEBUG_LEVEL 5
 #include <re_dbg.h>
 
-#include <QAudioOutput>
+class QtAudioPlayer : public QThread {
+private:
+	int frame_size;
 
-#define WRITE_BUFFERS  4
-#define INC_WPOS(a) ((a) = (((a) + 1) % WRITE_BUFFERS))
-
-class QtAudioPlayer {
-public:
-	struct dspbuf bufs[WRITE_BUFFERS];
-	int pos;
-	HWAVEOUT waveout;
-	bool rdy;
-	bool closed;
-	size_t inuse;
-	auplay_write_h *writer;
+	auplay_write_h *source;
 	void *arg;
 
-	QAudioOutput output;
+	std::shared_ptr<QAudioOutput> output;
+	QIODevice *device;
 
-	int dsp_write();
-	static void CALLBACK waveOutCallback_(HWAVEOUT hwo, UINT uMsg, DWORD_PTR dwInstance, DWORD_PTR dwParam1, DWORD_PTR dwParam2);
-	int write_stream_open(unsigned int dev, const auplay_prm *prm);
-	void waveOutCallback(HWAVEOUT hwo, UINT uMsg, DWORD_PTR dwParam1, DWORD_PTR dwParam2);
+protected:
+	void run()
+	{
+		std::vector<char> buf(frame_size * 2);
+		while (1) {
+			if (isInterruptionRequested()) break;
+			int n = output->bytesFree();
+			if (n < (int)buf.size()) {
+				msleep(1);
+			} else if (source((uint8_t *)&buf[0], buf.size(), arg)) {
+				device->write(&buf[0], buf.size());
+			}
+		}
+	}
+public:
+	~QtAudioPlayer()
+	{
+		requestInterruption();
+		wait();
+	}
+
+	void go(auplay_write_h *src, void *arg, int frame_size)
+	{
+		this->source = src;
+		this->arg = arg;
+		this->frame_size = frame_size;
+		QAudioFormat format;
+		format.setByteOrder(QAudioFormat::LittleEndian);
+		format.setChannelCount(1);
+		format.setCodec("audio/pcm");
+		format.setSampleRate(8000);
+		format.setSampleSize(16);
+		format.setSampleType(QAudioFormat::SignedInt);
+		output = std::shared_ptr<QAudioOutput>(new QAudioOutput(format));
+		device = output->start();
+		start();
+	}
 };
 
 struct auplay_st {
@@ -50,209 +77,27 @@ struct auplay_st {
 static void auplay_destructor(void *arg)
 {
 	auplay_st *st = (auplay_st *)arg;
-	int i;
-
-	st->obj.writer = NULL;
-
-	/* Mark the device for closing, and wait for all the
-	 * buffers to be returned by the driver
-	 */
-	st->obj.rdy = false;
-	while (st->obj.inuse > 0)
-		Sleep(50);
-
-	if (st->obj.waveout) {
-		waveOutReset(st->obj.waveout);
-	}
-
-	for (i = 0; i < WRITE_BUFFERS; i++) {
-		if (st->obj.waveout) {
-			waveOutUnprepareHeader(st->obj.waveout, &st->obj.bufs[i].wh, sizeof(WAVEHDR));
-		}
-		mem_deref(st->obj.bufs[i].mb);
-	}
-
-	if (st->obj.waveout) {
-		waveOutClose(st->obj.waveout);
-	}
-
-	for (i=0; i<100; i++) {
-		if (st->obj.closed) break;
-		Sleep(10);
-    }
-
 	mem_deref(st->ap);
-
 	st->~auplay_st();
-}
-
-int QtAudioPlayer::dsp_write()
-{
-	MMRESULT res;
-	WAVEHDR *wh;
-	struct mbuf *mb;
-
-	if (!rdy)
-		return EINVAL;
-
-	wh = &bufs[pos].wh;
-	if (wh->dwFlags & WHDR_PREPARED) {
-		return EINVAL;
-	}
-	mb = bufs[pos].mb;
-	wh->lpData = (LPSTR)mb->buf;
-
-	if (writer) {
-		writer(mb->buf, mb->size, arg);
-	}
-
-	wh->dwBufferLength = mb->size;
-	wh->dwFlags = 0;
-	wh->dwUser = (DWORD_PTR) mb;
-
-	if (waveout) {
-		waveOutPrepareHeader(waveout, wh, sizeof(*wh));
-	}
-
-	INC_WPOS(pos);
-
-	if (waveout) {
-		res = waveOutWrite(waveout, wh, sizeof(*wh));
-		if (res != MMSYSERR_NOERROR)
-			DEBUG_WARNING("dsp_write: waveOutWrite: failed: %08x\n", res);
-		else
-			inuse++;
-	}
-
-	return 0;
-}
-
-void QtAudioPlayer::waveOutCallback(HWAVEOUT hwo, UINT uMsg, DWORD_PTR dwParam1, DWORD_PTR dwParam2)
-{
-	WAVEHDR *wh = (WAVEHDR *)dwParam1;
-
-	(void)hwo;
-	(void)dwParam2;
-	switch (uMsg) {
-
-	case WOM_OPEN:
-		rdy = true;
-		break;
-
-	case WOM_DONE:
-		/*LOCK();*/
-		waveOutUnprepareHeader(waveout, wh, sizeof(*wh));
-		/*UNLOCK();*/
-		inuse--;
-		dsp_write();
-		break;
-
-	case WOM_CLOSE:
-		rdy = false;
-		closed = true;
-		break;
-
-	default:
-		break;
-	}
-}
-
-void CALLBACK QtAudioPlayer::waveOutCallback_(HWAVEOUT hwo, UINT uMsg, DWORD_PTR dwInstance, DWORD_PTR dwParam1, DWORD_PTR dwParam2)
-{
-	QtAudioPlayer *obj = (QtAudioPlayer *)dwInstance;
-	obj->waveOutCallback(hwo, uMsg, dwParam1, dwParam2);
-}
-
-static unsigned int find_dev(const char *dev)
-{
-	WAVEOUTCAPS wic;
-	unsigned int i;
-	unsigned int nInDevices = waveOutGetNumDevs();
-	for (i = 0; i < (int)nInDevices; i++) {
-		if (waveOutGetDevCaps(i, &wic, sizeof(WAVEOUTCAPS))==MMSYSERR_NOERROR) {
-			if (!strcmp(dev, wic.szPname)) {
-				return i;
-			}
-		}
-	}
-	return WAVE_MAPPER;
-}
-
-int QtAudioPlayer::write_stream_open(unsigned int dev, const struct auplay_prm *prm)
-{
-	WAVEFORMATEX wfmt;
-	MMRESULT res;
-	int i;
-
-	/* Open an audio I/O stream. */
-	waveout = NULL;
-	pos = 0;
-	rdy = false;
-	closed = false;
-
-	for (i = 0; i < WRITE_BUFFERS; i++) {
-		memset(&bufs[i].wh, 0, sizeof(WAVEHDR));
-		bufs[i].mb = mbuf_alloc(2 * prm->frame_size);
-	}
-
-	wfmt.wFormatTag      = WAVE_FORMAT_PCM;
-	wfmt.nChannels       = prm->ch;
-	wfmt.nSamplesPerSec  = prm->srate;
-	wfmt.wBitsPerSample  = 16;
-	wfmt.nBlockAlign     = (prm->ch * wfmt.wBitsPerSample) / 8;
-	wfmt.nAvgBytesPerSec = wfmt.nSamplesPerSec * wfmt.nBlockAlign;
-	wfmt.cbSize          = 0;
-
-	res = waveOutOpen(&waveout, dev, &wfmt,
-			  (DWORD_PTR) QtAudioPlayer::waveOutCallback_,
-			  (DWORD_PTR) this,
-			  CALLBACK_FUNCTION | WAVE_FORMAT_DIRECT);
-	if (res != MMSYSERR_NOERROR) {
-		DEBUG_WARNING("waveOutOpen: failed %d\n", res);
-//		return EINVAL;
-		waveout = NULL;
-	}
-
-	return 0;
 }
 
 int qtaudio_play_alloc(auplay_st **stp, struct auplay *ap, auplay_prm *prm, const char *device, auplay_write_h *wh, void *arg, void *user_data)
 {
-	auplay_st *st;
-	int i, err;
 	(void)device;
 	(void)user_data;
 
-	if (!stp || !ap || !prm)
-		return EINVAL;
+	if (!stp || !ap || !prm) return EINVAL;
 
-	st = (auplay_st *)mem_zalloc(sizeof(*st), auplay_destructor);
-	if (!st)
-		return ENOMEM;
-
-	new(st) auplay_st();
-
-	st->ap  = (struct auplay *)mem_ref(ap);
-	st->obj.writer  = wh;
-	st->obj.arg = arg;
+	auplay_st *st = (auplay_st *)mem_zalloc(sizeof(*st), auplay_destructor);
+	if (!st) return ENOMEM;
 
 	prm->fmt = AUFMT_S16LE;
 
-	err = st->obj.write_stream_open(find_dev(device), prm);
-	if (err)
-		goto out;
+	new(st) auplay_st();
+	st->ap  = (struct auplay *)mem_ref(ap);
+	st->obj.go(wh, arg, prm->frame_size);
 
-	/* The write runs at 100ms intervals
-	 * prepare enough buffers to suite its needs
-	 */
-	for (i = 0; i < 5; i++)
-		st->obj.dsp_write();
+	*stp = st;
 
- out:
-	if (err)
-		mem_deref(st);
-	else
-		*stp = st;
-
-	return err;
+	return 0;
 }
